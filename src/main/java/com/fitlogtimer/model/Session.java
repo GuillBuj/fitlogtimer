f1f12f3d1c17d3935d3c2a8fd798a35d866012e5
GuillBuj <guillaume.bujon@gmail.com>
2025-01-30 16:15:12 +0100

    config h2

--- src/main/java/com/fitlogtimer/model/Session.java
+++ src/main/java/com/fitlogtimer/model/Session.java
@@ -3,9 +3,13 @@ package com.fitlogtimer.model;
 import java.sql.Date;
 import java.util.List;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -21,5 +25,8 @@ public class Session {
     private Date date;
     private int bodyWeight;
     private String comment;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "session_id") // Clé étrangère dans ExerciseSet
     private List<ExerciseSet> setRecords;
 }

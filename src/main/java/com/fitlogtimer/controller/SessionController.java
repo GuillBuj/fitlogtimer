f4f9474e9a5381f5f30d6895b61655dab5d994ee
GuillaumeBuj <guillaume.bujon@gmail.com>
2025-03-12 11:41:12 +0100

    create session basique

--- src/main/java/com/fitlogtimer/controller/SessionController.java
+++ src/main/java/com/fitlogtimer/controller/SessionController.java
@@ -6,16 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fitlogtimer.dto.SessionDetailsDTO;
 import com.fitlogtimer.dto.SessionDetailsGroupedDTO;
 import com.fitlogtimer.dto.SessionGroupedDTO;
+import com.fitlogtimer.dto.SessionInDTO;
 import com.fitlogtimer.dto.SessionOutDTO;
 import com.fitlogtimer.model.Session;
 import com.fitlogtimer.service.SessionService;
@@ -55,17 +58,36 @@ public class SessionController {
         return "session-details"; // Nom de la vue HTML (session-details.html)
     }
 
-    // @GetMapping
-    // public String listSessions(Model model) {
-    //     List<Session> sessions = sessionService.getAllSessions();
-    //     if (sessions.size()>0){
-    //         log.info("-*-*-*-*-*-*-*-* Sessions: {}", sessions.toString());
-    //     } else {
-    //         log.info("-*-*-*-*-*-*-*-* Sessions: VIDE");
-    //     }
+    @GetMapping("/")
+    public String getSessionsList(Model model) {
+        List<Session> sessionsList = sessionService.getAllSessions();
+        if (sessionsList.size()>0){
+            log.info("-*-*-*-*-*-*-*-* Sessions: {}", sessionsList.toString());
+        } else {
+            log.info("-*-*-*-*-*-*-*-* Sessions: VIDE");
+        }
         
-    //     model.addAttribute("sessions", sessions);
-    //     return "session-list";
-    // }
+        model.addAttribute("sessionsList", sessionsList);
+        return "session-list";
+    }
+
+    @GetMapping("/new")
+    public String showSessionForm(Model model) {
+        model.addAttribute("session", new SessionInDTO(null, 0.0, ""));
+        return "session-form";
+    }
+
+    @PostMapping("/create")
+    public String createSession(@ModelAttribute("session") SessionInDTO sessionInDTO, RedirectAttributes redirectAttributes) {
+        Session session = new Session();
+        session.setDate(sessionInDTO.date());
+        session.setBodyWeight(sessionInDTO.bodyWeight());
+        session.setComment(sessionInDTO.comment());
+
+        sessionService.saveSession(session);
+
+        redirectAttributes.addFlashAttribute("successMessage", "Session créée avec succès !");
+        return "redirect:/sessions";
+    }
 
 }

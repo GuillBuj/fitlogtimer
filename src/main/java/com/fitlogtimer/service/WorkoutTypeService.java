fe540ba0fb5c623ee82dcc0ec157f7b186c55e92
GuillBuj <guillaume.bujon@gmail.com>
2025-06-12 13:35:31 +0200

    WorkoutType entity - mapping - correction service - seeder pour creer les types déjà en bdd(String->entity) - findOrCreate - correction create et update Workout

--- src/main/java/com/fitlogtimer/service/WorkoutTypeService.java
+++ src/main/java/com/fitlogtimer/service/WorkoutTypeService.java
@@ -1,11 +1,29 @@
 package com.fitlogtimer.service;
 
+import com.fitlogtimer.model.WorkoutType;
 import com.fitlogtimer.repository.WorkoutTypeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WorkoutTypeService {
     private final WorkoutTypeRepository workoutTypeRepository;
+
+    public WorkoutType findOrCreate(String name){
+
+        if(name == null || name.isBlank()){
+            log.error("WorkoutType name is null or empty");
+            return null;
+        }
+
+        return workoutTypeRepository.findByName(name)
+                .orElseGet(()-> {
+                    log.info("Creating new workout type {}", name);
+                    WorkoutType newType = new WorkoutType(name);
+                    return workoutTypeRepository.save(newType);
+                });
+    }
 }
